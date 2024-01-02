8495de941093e182a15b101c7f774ca65bf01d11
shenyanxiang <620775899@qq.com>
2024-01-02 11:14:26 +0800

    2024-1-2

--- client/src/components/views/Analysis.vue
+++ client/src/components/views/Analysis.vue
@@ -6,39 +6,129 @@
           <el-col :span="12" :offset="6">
             <el-card class="box-card">
               <div slot="header" class="clearfix">
-                <el-radio-group v-model="inputType" @change="handleChange">
+                <el-radio-group v-model="inputType" @change="handleInputChange">
                   <el-radio-button label="protein">Input Protein</el-radio-button>
                   <el-radio-button label="genome">Input Genome</el-radio-button>
                 </el-radio-group>
+              <br><br>
               </div>
               <div v-if="inputType === 'protein'">
-                <el-input placeholder="Please input protein"></el-input>
+                <el-form :model="proteinForm" label-position="top">
+                  <el-form-item label="Choose input method">
+                    <el-radio-group v-model="inputMethod">
+                      <el-radio label="text">Input sequence</el-radio>
+                      <el-radio label="file">Upload a fasta file</el-radio>
+                    </el-radio-group>
+                  </el-form-item>
+                  <el-form-item label="Input protein sequence(s)" v-if="inputMethod === 'text'">
+                    <el-input v-model="proteinForm.inputProtein" :autosize="{ minRows: 4, maxRows: 8 }" type="textarea" placeholder=">protein sequence 1 ..." clearable/>
+                  </el-form-item>
+                  <el-form-item label="Upload a protein FASTA format file" v-else>
+                    <el-upload
+                      ref="upload"
+                      :limit="1"
+                      :on-exceed="handleExceed"
+                      :auto-upload="false"
+                      :on-change="handleFileChange"
+                    >
+                      <template #trigger>
+                        <el-button slot="trigger" type="primary">select file</el-button>
+                      </template>
+                    </el-upload>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button type="primary" @click="onSubmit">Run</el-button>
+                    <el-button>Clear</el-button>
+                  </el-form-item>
+                </el-form>
               </div>
               <div v-else>
-                <el-input placeholder="Please input genome"></el-input>
+                <el-input placeholder="Please input genome"/>
               </div>
             </el-card>
           </el-col>
         </el-row>
+        <el-row><h1>{{ msg }}</h1></el-row>
       </el-main>
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue';
 import axios from 'axios';
+import { genFileId } from 'element-plus';
+import type { UploadInstance, UploadProps, UploadRawFile, UploadFile } from 'element-plus';
 
-export default {
-  data() {
-    return {
-      inputType: 'protein',
-    };
-  },
-  methods: {
-    handleChange(value: any) {
-      console.log('Input type: ' + value);
-    },
-  },
+const proteinForm = reactive<any>({
+  inputProtein: '',
+  file: {},
+});
+
+const upload = ref<UploadInstance>();
+const inputType = ref('protein');
+const inputMethod = ref('text');
+const msg = ref('');
+
+const handleExceed: UploadProps['onExceed'] = (files) => {
+  upload.value!.clearFiles();
+  const file = files[0] as UploadRawFile;
+  file.uid = genFileId();
+  upload.value!.handleStart(file);
 };
+
+const handleFileChange = (uploadFile: UploadFile) => {
+  proteinForm.file.value = uploadFile;
+  console.log('File:', proteinForm.file.value.raw);
+};
+
+const handleInputChange = (val: any) => {
+  inputType.value = val;
+}
+
+const getResources = () => {
+  const path = 'http://127.0.0.1:5001/api/analysis';
+  axios.get(path)
+    .then((res) => {
+      msg.value = res.data.sequence;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const onSubmit = () => {
+  if (inputMethod.value === 'file') {
+    const formData = new FormData();
+    if (proteinForm.file.value.raw) {
+      formData.append('file', proteinForm.file.value.raw);
+    } else {
+      console.error('No file selected');
+    }
+    formData.append('inputMethod', inputMethod.value);
+    console.log(formData);
+    axios.post('http://127.0.0.1:5001/api/analysis', formData)
+      .then((response) => {
+        console.log(response);
+        getResources();
+      }, (error) => {
+        console.log(error);
+      });
+  } else {
+    console.log(proteinForm.inputProtein);
+    axios.post('http://127.0.0.1:5001/api/analysis', {
+      inputProtein: proteinForm.inputProtein,
+      inputMethod: inputMethod.value,
+    })
+    .then((response) => {
+      console.log(response);
+      getResources();
+    }, (error) => {
+      console.log(error);
+    });
+  }
+};
+
+onMounted(getResources);
 </script>
 
 <style scoped>
--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 
 # configuration
@@ -14,44 +15,31 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-RESOURCES = [
-    {
-        'sn': '100',
-        'teacher': 'Mike',
-        'learnt': True
-    },
-    {
-        'sn': '101',
-        'teacher': 'John',
-        'learnt': False
-    },
-    {
-        'sn': '102',
-        'teacher': 'Walter',
-        'learnt': True
-    }
-]
-
-@app.route('/resources', methods=['GET', 'POST'])
-def all_res():
-    response_object = {'status': 'success'}
-    if request.method == 'POST':
-        post_data = request.get_json()
-        RESOURCES.append({
-            'sn': post_data.get('sn'),
-            'teacher': post_data.get('teacher'),
-            'learnt': post_data.get('learnt')
-        })
-        response_object['message'] = 'Resource added!'
-    else:
-        response_object['resources'] = RESOURCES
-    return jsonify(response_object)
-
 # sanity check route
 @app.route('/open', methods=['GET'])
 def open_door():
     return jsonify(u'Hello World!')
 
+last_sequence = 'imhere'
+@app.route('/api/analysis', methods=['GET', 'POST'])
+def analysis():
+    global last_sequence
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        input_method = request.form.get('inputMethod') or request.json.get('inputMethod')
+        if input_method == 'file':
+            file = request.files['file']
+            filename = secure_filename(file.filename)
+            file.save('/public/yxshen/DposFinderWeb/server/upload/' + filename)
+            last_sequence = 'upload success!!!'
+            return jsonify(response_object)
+        else:
+            post_data = request.get_json()
+            last_sequence = post_data.get('inputProtein')
+            return jsonify(response_object)
+    else:
+        response_object['sequence'] = last_sequence
+        return jsonify(response_object)
 
 if __name__ == '__main__':
     app.config['JSON_AS_ASCII'] = False
